bb1973fca1e74252f67d6316934610eccab05cf9
alicialbt <alicialebreton44@gmail.com>
2016-05-11 08:17:41 +0200

    Add files via upload

---
+++ first.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>TC Advisor</title>
+  <meta charset="utf-8">
+  <link href="site.css" rel="stylesheet">
+</head>
+
+
+<body>
+
+  <nav id="nav01"></nav>
+
+  <div id="first">
+
+  <h5>TC around the World</h5>
+    <p><img class="middle" src="images/sansfond.png" alt="connections" height="318" width="750" style="float:middle"/></p>
+    <footer id="foot01"></footer>
+  </div>
+
+<script src="script.js"></script>
+
+</body>
+</html>
+
