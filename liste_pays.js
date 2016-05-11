be043f07d3ada26f998c66e824984e8b35e81b1b
Jean Cassou-Mounat <jean.cassou.mounat@hotmail.fr>
2016-05-11 09:08:15 +0200

    Update liste_pays.js

--- liste_pays.js
+++ liste_pays.js
@@ -2,12 +2,13 @@ $(document).ready(function() {
     $.ajax({
         url: "liste_pays.php"
     }).then(function(data) {
-        
+
        for(var x=0; x<data.length; x++){
-       $('#dropdownpays').append(data[x].nom_pays);
+       $('#dropdownpays').append("<a href='site_pays_detail.php?nom_pays="+data[x].nom_pays+"'>"+data[x].nom_pays+"</a>");
        $('#dropdownpays').append("<br>");
        }
        console.log(data);
-       
+
     });
 });
+
