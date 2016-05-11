40139291fc7b0535a2d04bdcad2f0389df56a53b
alicialbt <alicialebreton44@gmail.com>
2016-05-11 08:17:23 +0200

    Add files via upload

---
+++ site_pays_detail.php
@@ -0,0 +1,36 @@
+CTYPE html>
+<html>
+<head>
+  <title>TC Advisor</title>
+  <meta charset="utf-8">
+  <link href="site.css" rel="stylesheet">
+  <script src="jquery.js"></script>        
+  <script src="liste_pays.js"></script>
+</head>
+
+<?php include("champ_connexion.php") ?>
+
+<body>
+  
+  <nav id = "entete">
+    <div id = "entete">
+      <h1>
+        <a href="first.php" style="text-decoration:none;">TC Around the World</a>
+        <img class="right" src="images/TCATW.png" alt="Travelling" height="200" width="220" style="float:right;margin:-80px 50px 0 20px;"/></h1>
+    </div>
+  </nav>
+  <nav id="nav01"></nav>
+
+  <div id="main">
+  <nav id="menu">
+    
+    <h1>Dans quelle ville voulez-vous partir?</h1>
+
+  </nav>  
+    <footer id="foot01"></footer>
+  </div>
+
+  <script src="script.js"></script>
+
+</body>
+</html>
